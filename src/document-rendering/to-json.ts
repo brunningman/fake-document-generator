867fa2f6e93c31ad41678fd6e2c8d210547d5a3a
Alex Brunner <abrunner@clearstory.build>
2025-11-05 12:28:24 -0800

    json shows empties

--- src/document-rendering/to-json.ts
+++ src/document-rendering/to-json.ts
@@ -11,7 +11,9 @@ export function renderToJson(shapedData: ShapedData): string {
       const header = headers[key];
       if (header) {
         const value = row[key];
-        if (value instanceof Date) {
+        if (value === null || value === undefined) {
+          rowData[header] = "";
+        } else if (value instanceof Date) {
           const month = (value.getMonth() + 1).toString().padStart(2, "0");
           const day = value.getDate().toString().padStart(2, "0");
           const year = value.getFullYear();
